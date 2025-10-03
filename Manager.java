bfd3f72655681c64d00b54f5a598f804ece185b6
Avika K <a.kapoor20@theabbey.co.uk>
2025-10-03 11:52:14 +0100

    Create Manager.java

---
+++ Manager.java
@@ -0,0 +1,11 @@
+public class Manager extends Person {
+    public float managerSalary;
+
+    public Manager(String name, int age, float managerSalary){
+        super(name,age);
+        this.managerSalary = managerSalary;
+    }
+
+    @Override
+
+}
